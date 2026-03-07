93695d362accc8803d140e7ea263705e6017f881
sergeidemianov <demyanovsk@elfin.tech>
2026-03-07 17:45:12 +0300

    change readme and fix code format

--- week1_python/day1_basics.py
+++ week1_python/day1_basics.py
@@ -1,12 +1,10 @@
 import re
 from collections import Counter
-from typing import Any
 
 
-def word_counter(text: str) -> dict[str,  int]:
+def word_counter(text: str) -> dict[str, int]:
     words = re.findall(r'\b\w+\b', text.lower())
     return dict(Counter(words))
-
 
 
 def remove_duplicates(items: list) -> list[int]:
@@ -21,18 +19,16 @@ def remove_duplicates(items: list) -> list[int]:
     return unique_list
 
 
-
-def three_elements(items: list) -> list[int] :
+def top_three_elements(items: list) -> list[int]:
     counter = Counter(items)
     return [item for item, _ in counter.most_common(3)]
 
 
-def revers_dict(data: dict[Any, Any]) -> dict[Any, Any]:
+def reverse_dict(data: dict[str, int]) -> dict[int, str]:
     return {v: k for k, v in data.items()}
 
 
-
-def group_by_letter(items: list[Any]) -> dict[Any, Any]:
+def group_by_letter(items: list[str]) -> dict[str, list[str]]:
     grouped = {}
     for item in items:
         first_letter = item[0].lower()
@@ -42,10 +38,10 @@ def group_by_letter(items: list[Any]) -> dict[Any, Any]:
     return grouped
 
 
-
 if __name__ == '__main__':
     print(word_counter("Привет, как у тебя дела? Привет, хорошо. Хорошо, это хорошо, когда хорошо."))
     print(remove_duplicates([1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 8, 8]))
-    print(three_elements([1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 6, 6, 8]))
-    print(revers_dict({'a': 1, 'b': 2, 'c': 3, 'd': 4}))
-    print(group_by_letter(['Привет', 'как,', 'у', 'тебя', 'дела', 'Привет', 'хорошо', 'Хорошо', 'это', 'хорошо', 'когда', 'хорошо']))
+    print(top_three_elements([1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 6, 6, 8]))
+    print(reverse_dict({'a': 1, 'b': 2, 'c': 3, 'd': 4}))
+    print(group_by_letter(
+        ['Привет', 'как,', 'у', 'тебя', 'дела', 'Привет', 'хорошо', 'Хорошо', 'это', 'хорошо', 'когда', 'хорошо']))
