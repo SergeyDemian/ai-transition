18e9fac231781b2a737c97ae99f91a8e20888d94
sergeidemianov <demyanovsk@elfin.tech>
2026-03-07 18:23:47 +0300

    add function for creating dictionary word by len

--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,10 +1,20 @@
-from week1_python.day2_collections import squares_of_even_numbers
+from week1_python.day2_collections import (
+    squares_of_even_numbers,
+    dictionary_for_word_len,
+)
 
 
 def test_squares_of_even_numbers():
     assert squares_of_even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9]) == [4, 16, 36, 64]
 
 
+def test_dictionary_for_word_len():
+    assert dictionary_for_word_len(
+        ["apple", "banana", "kiwi", "pear", "watermelon"]
+    ) == {"apple": 5, "banana": 6, "kiwi": 4, "pear": 4, "watermelon": 10}
+
+
 if __name__ == "__main__":
     test_squares_of_even_numbers()
+    test_dictionary_for_word_len()
     print("Everything passed")
--- week1_python/day2_collections.py
+++ week1_python/day2_collections.py
@@ -2,5 +2,10 @@ def squares_of_even_numbers(items: list[int]) -> list[int]:
     return [item**2 for item in items if item % 2 == 0]
 
 
+def dictionary_for_word_len(items: list[str]) -> dict[str, int]:
+    return {item: len(item) for item in items}
+
+
 if __name__ == "__main__":
     print(squares_of_even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    print(dictionary_for_word_len(["apple","banana","kiwi","pear","watermelon"]))
