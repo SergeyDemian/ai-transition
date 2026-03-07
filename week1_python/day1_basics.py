7abac516690ce179e436cf2db3ac45d5caf83688
sergeidemianov <demyanovsk@elfin.tech>
2026-03-07 17:33:36 +0300

    refactor day1 python basics solutions add readme and clean repository structure

--- week1_python/day1_basics.py
+++ week1_python/day1_basics.py
@@ -1,48 +1,51 @@
 import re
+from collections import Counter
+from typing import Any
 
-def word_counter():
-    frase = "Привет, как у тебя дела? Привет, хорошо. Хорошо, это хорошо, когда хорошо."
-    words = re.findall(r'\b\w+\b', frase.lower())
-    return dict((i, words.count(i)) for i in words)
 
-def remove_duplicates():
-    original_list = [1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 8, 8]
+def word_counter(text: str) -> dict[str,  int]:
+    words = re.findall(r'\b\w+\b', text.lower())
+    return dict(Counter(words))
+
+
+
+def remove_duplicates(items: list) -> list[int]:
     check_set = set()
     unique_list = []
-    for i in original_list:
+
+    for i in items:
         if i not in check_set:
             unique_list.append(i)
             check_set.add(i)
 
     return unique_list
 
-def three_elements():
-    original_list = [1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 6, 6, 8]
-    dictionary = dict((i, original_list.count(i)) for i in original_list)
-    sorted_dictionary = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True) [:3])
-    return list(sorted_dictionary)
 
 
-def revers_dict():
-    my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-    reversed_dict = {v: k for k, v in my_dict.items()}
-    return reversed_dict
+def three_elements(items: list) -> list[int] :
+    counter = Counter(items)
+    return [item for item, _ in counter.most_common(3)]
 
-def group_by_letter():
-    origin_list = ['Привет', 'как,', 'у', 'тебя', 'дела', 'Привет', 'хорошо', 'Хорошо', 'это', 'хорошо', 'когда', 'хорошо']
+
+def revers_dict(data: dict[Any, Any]) -> dict[Any, Any]:
+    return {v: k for k, v in data.items()}
+
+
+
+def group_by_letter(items: list[Any]) -> dict[Any, Any]:
     grouped = {}
-    for item in origin_list:
+    for item in items:
         first_letter = item[0].lower()
         if first_letter not in grouped.keys():
             grouped[first_letter] = []
-
         grouped[first_letter].append(item)
-
     return grouped
 
+
+
 if __name__ == '__main__':
-    print(word_counter())
-    print(remove_duplicates())
-    print(three_elements())
-    print(revers_dict())
-    print(group_by_letter())
+    print(word_counter("Привет, как у тебя дела? Привет, хорошо. Хорошо, это хорошо, когда хорошо."))
+    print(remove_duplicates([1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 8, 8]))
+    print(three_elements([1, 2, 4, 4, 5, 5, 5, 6, 1, 3, 6, 6, 6, 8]))
+    print(revers_dict({'a': 1, 'b': 2, 'c': 3, 'd': 4}))
+    print(group_by_letter(['Привет', 'как,', 'у', 'тебя', 'дела', 'Привет', 'хорошо', 'Хорошо', 'это', 'хорошо', 'когда', 'хорошо']))
