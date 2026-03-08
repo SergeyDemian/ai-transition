9392db2ddd5f73510013ef7e90694df2e0476642
sergeidemianov <demyanovsk@elfin.tech>
2026-03-08 18:03:21 +0300

    add 5 day solution

---
+++ week1_python/day5_pandas_analysis/pandas_analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def main() -> None:
+    df = pd.read_csv("dataset.csv")
+    print(df)
+
+    print(df.head(2))
+    print(df.info())
+    print(df.describe())
+
+    print(df["age"].mean())
+    print(df["salary"].mean())
+
+    print(df[df["salary"] > 65000])
+    print(df[df["experience"] > 5])
+    print(df.groupby("city")["salary"].mean())
+    print(df.sort_values("salary", ascending=False).head(3))
+    df["salary_per_experience"] = df["salary"] / df["experience"]
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
